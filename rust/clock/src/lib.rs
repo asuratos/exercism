ff5eb1bd9c0933d803b87e4c1406ce0573beb7cb
Aldrich Suratos <cyurio@gmail.com>
2021-01-09 22:54:36 +0900

    clock done

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,52 +1,30 @@
 use std::fmt;
 
+use chrono::{Duration, NaiveTime};
+
 #[derive(Debug, PartialEq)]
 pub struct Clock {
-    hr : i32,
-    min: i32,
+    time: NaiveTime,
 }
 
 impl Clock {
-    // time adjustment functions
-
-    fn rollover(hours: i32, minutes: i32) -> (i32, i32) {
-        // perform rollovers
-        let mut hours_from_mins: i32;
-        let mut clock_mins: i32;
-        let mut clock_hour: i32;
-
-        if minutes >= 0 {
-            hours_from_mins = minutes / 60;
-            clock_mins = minutes % 60;
-        } else {
-            hours_from_mins = ((-1 * minutes) / 60) * -1 ;
-            clock_mins = (hours_from_mins * -1 * 60) + minutes;
-        }
-
-        clock_hour = (hours + hours_from_mins) % 24;
-        if clock_hour < 0 {
-            clock_hour = (24 - (clock_hour * -1)) % 24;
-        }
-
-        (clock_hour, clock_mins)
-    }
-
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let clock_time = Self::rollover(hours, minutes); 
-
-        Clock{
-            hr: clock_time.0,
-            min: clock_time.1,
+        Clock {
+            time: NaiveTime::from_hms(0, 0, 0)
+                + Duration::hours(hours as i64)
+                + Duration::minutes(minutes as i64),
         }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        unimplemented!("Add {} minutes to existing Clock time", minutes);
+        Clock {
+            time: self.time + Duration::minutes(minutes as i64),
+        }
     }
 }
 
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{:02}:{:02}", self.hr, self.min)
+        write!(f, "{}", self.time.format("%H:%M"))
     }
 }
