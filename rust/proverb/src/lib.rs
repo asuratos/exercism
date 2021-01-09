f19b92c2ffd2f34d53013f9244be052959423701
Aldrich Suratos <cyurio@gmail.com>
2021-01-10 00:56:00 +0900

    proverb finished

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,3 +1,19 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    unimplemented!("build a proverb from this list of items: {:?}", list)
+    let mut proverb = Vec::new();
+
+    if list.len() == 0 {
+        return String::new();
+    }
+
+    for i in 0..list.len() - 1 {
+        proverb.push(format!(
+            "For want of a {} the {} was lost.",
+            list[i],
+            list[i + 1]
+        ));
+    }
+
+    proverb.push(format!("And all for the want of a {}.", list[0]));
+
+    proverb.join("\n")
 }
