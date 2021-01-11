f1177cae3a323e96581d0ed22697c2084ec0a2f3
Aldrich Suratos <cyurio@gmail.com>
2021-01-11 23:04:09 +0900

    finish space-age

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -1,38 +1,35 @@
-// The code below is a stub. Just enough to satisfy the compiler.
-// In order to pass the tests you can add-to or change any of this code.
-
 #[derive(Debug)]
-pub struct Duration;
+pub struct Duration {
+    secs: f64,
+}
 
 impl From<u64> for Duration {
     fn from(s: u64) -> Self {
-        unimplemented!("s, measured in seconds: {}", s)
+        Duration { secs: s as f64 }
     }
 }
 
 pub trait Planet {
-    fn years_during(d: &Duration) -> f64 {
-        unimplemented!(
-            "convert a duration ({:?}) to the number of years on this planet for that duration",
-            d,
-        );
-    }
+    fn years_during(d: &Duration) -> f64;
 }
 
-pub struct Mercury;
-pub struct Venus;
-pub struct Earth;
-pub struct Mars;
-pub struct Jupiter;
-pub struct Saturn;
-pub struct Uranus;
-pub struct Neptune;
+macro_rules! planet_s2y_conv {
+    ($planet:ident, $p:ty, $earthyear_mod:literal) => {
+        pub struct $planet;
 
-impl Planet for Mercury {}
-impl Planet for Venus {}
-impl Planet for Earth {}
-impl Planet for Mars {}
-impl Planet for Jupiter {}
-impl Planet for Saturn {}
-impl Planet for Uranus {}
-impl Planet for Neptune {}
+        impl Planet for $p {
+            fn years_during(d: &Duration) -> f64 {
+                (d.secs / (31_557_600_f64)) / ($earthyear_mod)
+            }
+        }
+    };
+}
+
+planet_s2y_conv!(Mercury, Mercury, 0.2408467_f64);
+planet_s2y_conv!(Venus, Venus, 0.61519726_f64);
+planet_s2y_conv!(Earth, Earth, 1_f64);
+planet_s2y_conv!(Mars, Mars, 1.8808158_f64);
+planet_s2y_conv!(Jupiter, Jupiter, 11.862615_f64);
+planet_s2y_conv!(Saturn, Saturn, 29.447498_f64);
+planet_s2y_conv!(Uranus, Uranus, 84.016846_f64);
+planet_s2y_conv!(Neptune, Neptune, 164.79132_f64);
