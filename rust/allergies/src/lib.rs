50a66a31966322395a8ceb54bc48bb55cd70abcd
Aldrich Suratos <cyurio@gmail.com>
2022-04-20 10:39:27 +0900

    Finish allergies, get magazine_cutout

--- rust/allergies/src/lib.rs
+++ rust/allergies/src/lib.rs
@@ -1,4 +1,6 @@
-pub struct Allergies;
+pub struct Allergies {
+    score: u32,
+}
 
 #[derive(Debug, PartialEq)]
 pub enum Allergen {
@@ -12,22 +14,37 @@ pub enum Allergen {
     Cats,
 }
 
+const ALL_ALLERGENS: [Allergen; 8] = [
+    Allergen::Eggs,
+    Allergen::Peanuts,
+    Allergen::Shellfish,
+    Allergen::Strawberries,
+    Allergen::Tomatoes,
+    Allergen::Chocolate,
+    Allergen::Pollen,
+    Allergen::Cats,
+];
+
 impl Allergies {
     pub fn new(score: u32) -> Self {
-        unimplemented!(
-            "Given the '{}' score, construct a new Allergies struct.",
-            score
-        );
+        Allergies { score }
     }
 
     pub fn is_allergic_to(&self, allergen: &Allergen) -> bool {
-        unimplemented!(
-            "Determine if the patient is allergic to the '{:?}' allergen.",
-            allergen
-        );
+        self.allergies().contains(allergen)
     }
 
     pub fn allergies(&self) -> Vec<Allergen> {
-        unimplemented!("Return the list of allergens contained within the score with which the Allergies struct was made.");
+        let bin = format!("{:08b}", self.score);
+
+        let mut allergens = vec![];
+
+        for (digit, allergen) in bin.chars().rev().zip(ALL_ALLERGENS) {
+            if digit == '1' {
+                allergens.push(allergen)
+            }
+        }
+
+        allergens
     }
 }
