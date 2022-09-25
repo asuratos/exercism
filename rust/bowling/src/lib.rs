46d948cfd0b109d956b020117d0e210f3257a153
Aldrich Suratos <cyurio@gmail.com>
2022-09-25 22:03:08 +0900

    new attempt at bowling

--- rust/bowling/src/lib.rs
+++ rust/bowling/src/lib.rs
@@ -4,139 +4,182 @@ pub enum Error {
     GameComplete,
 }
 
-#[derive(Clone, Debug)]
-enum Roll {
+// Enum for recording the final state of a frame
+#[derive(Debug)]
+enum FinishedFrame {
     Strike,
     Spare(u16),
-    Open(u16),
-    Bonus(u16),
+    Normal((u16, u16)),
 }
 
-impl Roll {
-    pub fn value(&self) -> u16 {
-        match self {
-            Roll::Strike => 10,
-            Roll::Spare(i) => *i,
-            Roll::Open(i) => *i,
-            Roll::Bonus(i) => *i,
+impl FinishedFrame {
+    pub fn first(&self) -> u16 {
+        match *self {
+            FinishedFrame::Strike => 10,
+            FinishedFrame::Spare(r1) => r1,
+            FinishedFrame::Normal((r1, _)) => r1,
+        }
+    }
+
+    pub fn total(&self) -> u16 {
+        match *self {
+            FinishedFrame::Strike => 10,
+            FinishedFrame::Spare(_) => 10,
+            FinishedFrame::Normal((r1, r2)) => r1 + r2,
         }
     }
 }
 
+// default kind of frame for frames 1-9, can only be rolled up to twice
+#[derive(Debug)]
+struct NormalFrame {
+    downed: u16,
+    rolled: bool,
+}
+
+impl NormalFrame {
+    pub fn new() -> Self {
+        NormalFrame {
+            downed: 0,
+            rolled: false,
+        }
+    }
+    fn roll(&mut self, roll: u16) -> Result<Option<FinishedFrame>, Error> {
+        if roll > (10 - self.downed) {
+            return Err(Error::NotEnoughPinsLeft);
+        }
+
+        if self.rolled {
+            let total = self.downed + roll;
+            if total == 10 {
+                return Ok(Some(FinishedFrame::Spare(self.downed)));
+            } else {
+                return Ok(Some(FinishedFrame::Normal((self.downed, roll))));
+            }
+        } else {
+            if roll == 10 {
+                return Ok(Some(FinishedFrame::Strike));
+            } else {
+                self.rolled = true;
+                self.downed = roll;
+            }
+        }
+
+        Ok(None)
+    }
+}
+
+#[derive(Debug)]
+struct LastFrame {
+    rolls: Vec<u16>,
+    fill: bool,
+    remaining: u16,
+}
+
+impl LastFrame {
+    pub fn new() -> Self {
+        LastFrame {
+            rolls: vec![],
+            fill: false,
+            remaining: 10,
+        }
+    }
+
+    pub fn is_finished(&self) -> bool {
+        (self.rolls.len() == 2 && !self.fill) || (self.rolls.len() == 3 && self.fill)
+    }
+
+    pub fn total(&self) -> u16 {
+        self.rolls.iter().sum()
+    }
+
+    pub fn roll(&mut self, roll: u16) -> Result<(), Error> {
+        if self.is_finished() {
+            return Err(Error::GameComplete);
+        }
+
+        if self.rolls.len() == 1 {
+            if roll > self.remaining {
+                return Err(Error::NotEnoughPinsLeft);
+            }
+
+            self.rolls.push(roll);
+
+            if roll == self.remaining {
+                self.fill = true;
+                self.remaining = 10;
+            }
+        } else {
+            self.rolls.push(roll);
+            if roll != 10 {
+                self.remaining -= roll;
+            } else {
+                self.fill = true;
+            }
+        }
+
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
 pub struct BowlingGame {
-    frames: Vec<Roll>,
-    frame: u16,
-    rolls_this_frame: u16,
-    remaining_pins: u16,
-    bonus: bool,
+    previous_frames: Vec<FinishedFrame>,
+    current_frame: NormalFrame,
+    final_frame: LastFrame,
 }
 
 impl BowlingGame {
     pub fn new() -> Self {
         BowlingGame {
-            frames: vec![],
-            frame: 1,
-            rolls_this_frame: 0,
-            remaining_pins: 10,
-            bonus: false,
+            previous_frames: vec![],
+            current_frame: NormalFrame::new(),
+            final_frame: LastFrame::new(),
         }
     }
 
-    pub fn roll(&mut self, pins: u16) -> Result<(), Error> {
-        if self.frame == 11 {
-            return Err(Error::GameComplete);
-        }
-
-        if pins > self.remaining_pins {
+    pub fn roll(&mut self, roll: u16) -> Result<(), Error> {
+        if roll > 10 {
             return Err(Error::NotEnoughPinsLeft);
         }
 
-        self.rolls_this_frame += 1;
+        if self.previous_frames.len() < 9 {
+            let rollres = self.current_frame.roll(roll)?;
 
-        if pins == 10 {
-            // special case, strike on very last roll
-            if self.bonus {
-                self.frames.push(Roll::Bonus(10));
-                self.next_frame();
-            } else if self.frame == 10 && self.rolls_this_frame == 1 {
-                self.frames.push(Roll::Strike);
-                self.rolls_this_frame = 0;
-                self.remaining_pins = 10;
-                self.bonus = true;
-            } else {
-                // if it's a strike
-                self.frames.push(Roll::Strike);
-                self.next_frame();
+            if let Some(finished) = rollres {
+                self.previous_frames.push(finished);
+                self.current_frame = NormalFrame::new();
             }
-            return Ok(());
-        } else if pins == self.remaining_pins {
-            // special case, spare on very last roll
-            if self.bonus {
-                self.frames.push(Roll::Bonus(pins));
-                self.next_frame();
-            } else if self.frame == 10 && self.rolls_this_frame == 2 {
-                self.frames.push(Roll::Spare(pins));
-                self.rolls_this_frame -= 1;
-                self.remaining_pins = 10;
-                self.bonus = true;
-            } else {
-                // if its a spare
-                self.frames.push(Roll::Spare(pins));
-                self.next_frame();
-            }
-            return Ok(());
+        } else if !self.final_frame.is_finished() {
+            self.final_frame.roll(roll)?;
         } else {
-            if self.bonus {
-                self.frames.push(Roll::Bonus(pins));
-            } else {
-                self.frames.push(Roll::Open(pins));
-            }
-        }
-        self.remaining_pins -= pins;
-        if self.rolls_this_frame == 2 {
-            self.next_frame();
+            return Err(Error::GameComplete);
         }
         Ok(())
     }
 
     pub fn score(&self) -> Option<u16> {
-        println!("{:?}", self.frames);
-        println!("{:?}", self.frame);
-        if self.frame != 11 {
-            return None;
+        // println!("{:?}", self);
+        if self.previous_frames.len() == 9 && self.final_frame.is_finished() {
+            let mut total = self.final_frame.total();
+
+            let mut prev_frame: Option<&FinishedFrame> = None;
+
+            for frame in &self.previous_frames {
+                total += frame.total();
+
+                total += match prev_frame {
+                    Some(&FinishedFrame::Strike) => frame.total(),
+                    Some(&FinishedFrame::Spare(_)) => frame.first(),
+                    _ => 0,
+                };
+
+                prev_frame = Some(frame);
+            }
+
+            return Some(total);
         }
 
-        let mut frames = self.frames.clone();
-        frames.push(Roll::Open(0));
-        frames.push(Roll::Open(0));
-
-        let score = frames
-            .windows(3)
-            .map(|window| match window[0] {
-                Roll::Strike => &window[..],
-                Roll::Spare(_) => &window[..=1],
-                _ => &window[0..1],
-                // Roll::Open(_) => &window[0..1],
-                // Roll::Bonus(i) => &[Roll::Open(i)]
-            })
-            .filter(|window| {
-                !window.iter().all(|roll| match roll {
-                    Roll::Bonus(_) => true,
-                    _ => false,
-                })
-            })
-            .map(|window| {
-                println!("{:?}", window);
-                window.iter().map(|roll| roll.value()).sum::<u16>()
-            })
-            .sum();
-
-        Some(score)
-    }
-
-    fn next_frame(&mut self) {
-        self.frame += 1;
-        self.rolls_this_frame = 0;
-        self.remaining_pins = 10;
+        None
     }
 }
