3989cc1a7fa35ca2007962de2bfda87777b5b04f
Aldrich Suratos <cyurio@gmail.com>
2022-09-29 09:57:41 +0900

    submit queen attack

--- rust/queen-attack/src/lib.rs
+++ rust/queen-attack/src/lib.rs
@@ -1,31 +1,47 @@
 #[derive(Debug)]
-pub struct ChessPosition;
+pub struct ChessPosition((i32, i32));
 
 #[derive(Debug)]
-pub struct Queen;
+pub struct Queen(ChessPosition);
 
 impl ChessPosition {
     pub fn new(rank: i32, file: i32) -> Option<Self> {
-        unimplemented!(
-            "Construct a ChessPosition struct, given the following rank, file: ({}, {}). If the position is invalid return None.",
-            rank,
-            file
-        );
+        if !(0..8).contains(&rank) || !(0..8).contains(&file) {
+            return None;
+        }
+
+        Some(ChessPosition((rank, file)))
+    }
+
+    pub fn rank(&self) -> i32 {
+        self.0 .0
+    }
+    pub fn file(&self) -> i32 {
+        self.0 .1
     }
 }
 
 impl Queen {
     pub fn new(position: ChessPosition) -> Self {
-        unimplemented!(
-            "Given the chess position {:?}, construct a Queen struct.",
-            position
-        );
+        Queen(position)
+    }
+
+    pub fn rank(&self) -> i32 {
+        self.0.rank()
+    }
+
+    pub fn file(&self) -> i32 {
+        self.0.file()
     }
 
     pub fn can_attack(&self, other: &Queen) -> bool {
-        unimplemented!(
-            "Determine if this Queen can attack the other Queen {:?}",
-            other
-        );
+        self.rank() == other.rank() || self.file() == other.file() || self.is_diagonal(other)
+    }
+
+    pub fn is_diagonal(&self, other: &Queen) -> bool {
+        let rank_d = self.rank() - other.rank();
+        let file_d = self.file() - other.file();
+
+        rank_d.abs() == file_d.abs()
     }
 }
